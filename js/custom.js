af5676517009343076d9f0079792e163eaab5a32
lanht <fedor@live.com.au>
2016-12-29 00:22:42 +1100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,112 +1,84 @@
-
- /* jQuery Pre loader
+/* jQuery Pre loader
   -----------------------------------------------*/
 $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
+
+    var lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+    if (lang == "zh-Hans") {
+        document.getElementById('body').style.fontFamily = '"Hiragino Sans GB W6", "helvetica-normal", sans-serif';
+    } else if (lang == "en") {
+        document.getElementById('body').style.fontFamily = '"helvetica-normal", "Hiragino Sans GB W6", sans-serif';
+    }
+
 });
-
-
- /* Google Map
+ 
+ 
+/* Body Font
 -----------------------------------------------*/
-var map = '';
-var center;
 
-function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
-      scrollwheel: false
-    };
-  
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
-  
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
-}
-
-function calculateCenter() {
-  center = map.getCenter();
-}
-
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
-
-$(function(){
-  loadGoogleMap();
-});
-
-
+ 
 /* Istope Portfolio
 -----------------------------------------------*/
 jQuery(document).ready(function($){
-
-  if ( $('.iso-box-wrapper').length > 0 ) { 
-
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
-
+ 
+  if ( $('.iso-box-wrapper').length > 0 ) {
+ 
+        var $container  = $('.iso-box-wrapper'),
+            $imgs     = $('.iso-box img');
+ 
       $container.imagesLoaded(function () {
-
+ 
         $container.isotope({
         layoutMode: 'fitRows',
         itemSelector: '.iso-box'
         });
-
+ 
         $imgs.load(function(){
           $container.isotope('reLayout');
         })
-
+ 
       });
-
+ 
       //filter items on button click
-
+ 
       $('.filter-wrapper li a').click(function(){
-
+ 
           var $this = $(this), filterValue = $this.attr('data-filter');
-
-      $container.isotope({ 
+ 
+      $container.isotope({
         filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
-
-      // don't proceed if already selected 
-
-      if ( $this.hasClass('selected') ) { 
-        return false; 
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false,
+        }               
+      });            
+ 
+      // don't proceed if already selected
+ 
+      if ( $this.hasClass('selected') ) {
+        return false;
       }
-
+ 
       var filter_wrapper = $this.closest('.filter-wrapper');
       filter_wrapper.find('.selected').removeClass('selected');
       $this.addClass('selected');
-
+ 
         return false;
-      }); 
-
+      });
+ 
   }
-
+ 
 });
-
-
- /* Navigation Bar
+ 
+ 
+/* Navigation Bar
   -----------------------------------------------*/
-$(document).ready(function(){ 
+$(document).ready(function(){
     "use strict";
-
+ 
     // Navbar Sticky
-
+ 
     (function() {
         var docElem = document.documentElement,
             didScroll = false,
@@ -120,7 +92,7 @@ $(document).ready(function(){
                 }
             }, false );
         }
-        
+       
         function scrollPage() {
             var sy = scrollY();
             if ( sy >= stickynav ) {
@@ -131,109 +103,160 @@ $(document).ready(function(){
             }
             didScroll = false;
         }
-        
+       
         function scrollY() {
             return window.pageYOffset || docElem.scrollTop;
-        }        
-        init();        
+        }       
+        init();       
     })();
-
+ 
 });
-
-
+ 
+ 
 $(document).ready(function(){
-            
+           
     "use strict";
-
+ 
     $('.menu-container').each(function(index) {
         $(this).find('.circle').attr('menu-link', index);
         $(this).find('.list-menu').clone().appendTo('body').attr('menu-link', index);
     });
-
+ 
     $('.menu-container .circle').click(function() {
         var linkedVideo = $('section').closest('body').find('.list-menu[menu-link="' + $(this).attr('menu-link') + '"]');
         linkedVideo.toggleClass('reveal-modal');
-       
+      
     });
-
+ 
     $('section').closest('body').find('.close-iframe').click(function() {
         $(this).closest('.list-menu').toggleClass('reveal-modal');
     });
-    
+   
  /* Home Slideshow Vegas
   -----------------------------------------------*/
   $(function() {
     $('.header-one').vegas({
         slides: [
-            { src: 'images/profile2.jpeg' },
-            { src: 'images/profile3.jpeg' },
-            { src: 'images/profile4.jpeg' }
+            { src: '../images/profile2.jpeg' },
+            { src: '../images/profile3.jpeg' },
+            { src: '../images/profile4.jpeg' }
         ],
         timer: false,
         transition: [ 'zoomIn', ],
         animation: ['kenburns']
     });
   });
-
+ 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
-
+ 
   });
-
-
-
+ 
+ 
+/* Overlay
+-----------------------------------------------*/ 
 function overlayOn(image) {
     var body = document.body;
     var overlay = document.querySelector('.portfolio-overlay');
     var img_src = image.src;
-     
-    document.getElementById("single-painting").src = image.src; 
-     
-    /* Toggle the aria-hidden state on the overlay and the 
+    var img_alt = image.alt;
+    var lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+    
+    document.getElementById("single-painting").src = image.src;
+    document.getElementById("single-painting").alt = image.alt;
+    document.getElementById("img-id").href = image.src;
+ 
+    if (lang == "zh-Hans") {
+        if (img_alt == "up1") {
+            document.getElementById("name").innerHTML = "上山虎 I";
+            document.getElementById("date").innerHTML = "2016.04";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down1") {
+            document.getElementById("name").innerHTML = "下山虎 I";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down2") {
+            document.getElementById("name").innerHTML = "下山虎 II";
+            document.getElementById("date").innerHTML = "2015.08";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "lay1") {
+            document.getElementById("name").innerHTML = "卧虎 I";
+            document.getElementById("date").innerHTML = "2015.08";
+            document.getElementById("size").innerHTML = "96 x 50 cm";
+ 
+        } else if (img_alt == "down3") {
+            document.getElementById("name").innerHTML = "下山虎 III";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down4") {
+            document.getElementById("name").innerHTML = "下山虎 IV";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+        } else {
+            alert ("Image not found!");
+        }
+    } else if (lang = "en") {
+        if (img_alt == "up1") {
+            document.getElementById("name").innerHTML = "Tiger going uphill I";
+            document.getElementById("date").innerHTML = "2016.04";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down1") {
+            document.getElementById("name").innerHTML = "Tiger going downhill I";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down2") {
+            document.getElementById("name").innerHTML = "Tiger going downhill II";
+            document.getElementById("date").innerHTML = "2015.08";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "lay1") {
+            document.getElementById("name").innerHTML = "Tiger sitting still I";
+            document.getElementById("date").innerHTML = "2015.08";
+            document.getElementById("size").innerHTML = "96 x 50 cm";
+ 
+        } else if (img_alt == "down3") {
+            document.getElementById("name").innerHTML = "Tiger going downhill III";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+ 
+        } else if (img_alt == "down4") {
+            document.getElementById("name").innerHTML = "Tiger going downhill IV";
+            document.getElementById("date").innerHTML = "2015.09";
+            document.getElementById("size").innerHTML = "135 x 70 cm";
+        } else {
+                                                alert ("Image not found!");
+                                }
+    }
+    
+    /* Toggle the aria-hidden state on the overlay and the
     no-scroll class on the body */
-    overlay.setAttribute('aria-hidden', 'false');
-    body.classList.toggle('noscroll', 'true');
+    overlay.setAttribute('aria-hidden', false);
+    body.classList.toggle('noscroll', true);
     /* On some mobile browser when the overlay was previously
-        opened and scrolled, if you open it again it doesn't 
-        reset its scrollTop property */
-        overlay.scrollTop = 0;
+    opened and scrolled, if you open it again it doesn't
+    reset its scrollTop property */
+    overlay.scrollTop = 0;
 };
-
-
+ 
+ 
 function overlayOff() {
     var body = document.body;
     var overlay = document.querySelector('.portfolio-overlay');
-    
-    /* Toggle the aria-hidden state on the overlay and the 
+   
+    /* Toggle the aria-hidden state on the overlay and the
     no-scroll class on the body */
-    overlay.setAttribute('aria-hidden', 'true');
-    body.classList.toggle('noscroll', 'false');
-    
-        /* On some mobile browser when the overlay was previously
-        opened and scrolled, if you open it again it doesn't 
-        reset its scrollTop property */
-        overlay.scrollTop = 0;  
+    overlay.setAttribute('aria-hidden', true);
+    body.classList.toggle('noscroll', false);
+   
+    /* On some mobile browser when the overlay was previously
+    opened and scrolled, if you open it again it doesn't
+    reset its scrollTop property */
+    overlay.scrollTop = 0; 
 };
-
-
-// <script>
-//  /* Pass image info when click
-//   -----------------------------------------------*/
-//   function getSrc(image){
-//     var img_src = image.src;
-//     //alert(img_src);
-//     var img_id = img_src.substring(img_src.lastIndexOf('/')+1,img_src.lastIndexOf('.'));
-//     //alert(img_id);
-//     document.location="single-project.html?" + img_id;
-//   };
-  
-// </script>
-
-// <script>
-//   function eng(){
-//       document.getElementById("intro").innerHTML = "Bingqing Lan, born in 1959 in Harbin, China, parents originally from Xiushan County, Chongqing. His father Shibi Lan (1915 - 1984) was a distinctive art graduate from The National University of China in the early 1930s. Guided and recognised by some of the most influential artists in the modern Chinese art history, including Beihong Xu, Junbi Huang, Shuqi Zhang, Fozhi Chen, Baoshi Fu, Shibi mastered many aspects of traditional Chinese art, including 'water-and-ink' paiting of landscape, portrait and figures, flowers and planations, animals etc, together with calligraphy and poetry. Shibi held his own exhibition in Chongqing China in March 1946. Although Bingqing Lan, under the influence of his father, has also developed his artistic mindset and skills in the early years, he did not follow the path of becoming a professional artist. Since the approach of his retirement in recent years, Bingqing has decided to pick up the paint brushes again, research and practise. Starting from tigers, the category that has been practised the most by him and his father, Bingqing is determined to set his foot back on track to explore the beauty of traditional Chinese painting, and wishes to share his journey and outcome with anyone who come across.";
-//   };
- 
-// </script>
-
